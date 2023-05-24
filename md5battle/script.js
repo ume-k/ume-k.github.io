8307962be34e19c5468e9f8279c33dbc3d1b994f
ume <umengn99@gmail.com>
2023-05-25 08:40:33 +0900

    update

--- md5battle/script.js
+++ md5battle/script.js
@@ -7,19 +7,18 @@ function calculateMD5() {
     const total2 = sumDigits(md5_2);
     let resultText;
 
-    document.getElementById("result").innerHTML = "バトル中！";
+    document.getElementById("result").innerHTML = "<div style='color:red'>⚔戦闘中⚔</div>";
     window.setTimeout(() => {
-        document.getElementById("result").innerHTML = "わあ２";
         document.getElementById("out-1").innerHTML = md5_1;
         document.getElementById("out-2").innerHTML = md5_2;
         document.getElementById("sum-1").innerHTML = total1;
         document.getElementById("sum-2").innerHTML = total2;
         if (total1 > total2) {
-            resultText = `${input1} が勝ち！`;
+            resultText = `結果……<br> 「${input1}」 が勝ち！`;
         } else if (total1 < total2) {
-            resultText = `${input2} が勝ち！`;
+            resultText = `結果……<br> 「${input2}」 が勝ち！`;
         } else {
-            resultText = "引き分け！";
+            resultText = "結果……<br>引き分け！";
         }
         document.getElementById("result").innerHTML = resultText;
     }, 800);
