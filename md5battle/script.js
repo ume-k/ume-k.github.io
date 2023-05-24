ff461ed67dcc29b8fdc42b0b721497d45944ed10
ume <umengn99@gmail.com>
2023-05-24 19:37:18 +0900

    update

--- md5battle/script.js
+++ md5battle/script.js
@@ -6,23 +6,24 @@ function calculateMD5() {
     const total1 = sumDigits(md5_1);
     const total2 = sumDigits(md5_2);
     let resultText;
-    document.getElementById("out-1").innerHTML = md5_1;
-    document.getElementById("out-2").innerHTML = md5_2;
-    document.getElementById("sum-1").innerHTML = total1;
-    document.getElementById("sum-2").innerHTML = total2;
 
+    document.getElementById("result").innerHTML = "バトル中！";
+    window.setTimeout(() => {
+        document.getElementById("result").innerHTML = "わあ２";
+        document.getElementById("out-1").innerHTML = md5_1;
+        document.getElementById("out-2").innerHTML = md5_2;
+        document.getElementById("sum-1").innerHTML = total1;
+        document.getElementById("sum-2").innerHTML = total2;
+        if (total1 > total2) {
+            resultText = `${input1} が勝ち！`;
+        } else if (total1 < total2) {
+            resultText = `${input2} が勝ち！`;
+        } else {
+            resultText = "引き分け！";
+        }
+        document.getElementById("result").innerHTML = resultText;
+    }, 800);
 
-    // document.getElementById("out-1").innerHTML = md5_1;
-    // document.getElementById("out-2").innerHTML = md5_2;
-
-    if (total1 > total2) {
-        resultText = `${input1}が勝ち！`;
-    } else if (total1 < total2) {
-        resultText = `${input2}が勝ち！`;
-    } else {
-        resultText = "引き分け！";
-    }
-    document.getElementById("result").innerHTML = resultText;
 }
 
 function sumDigits(str) {
